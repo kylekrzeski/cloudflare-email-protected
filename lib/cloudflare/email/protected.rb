df2779ff2279e6ce68cc8b89c8a0b5e0744ecdd7
Łukasz Śliwa <lukasz.sliwa@gmail.com>
2023-07-13 14:53:07 +0200

    Standard fix

--- lib/cloudflare/email/protected.rb
+++ lib/cloudflare/email/protected.rb
@@ -8,16 +8,16 @@ module Cloudflare
       class Error < StandardError; end
 
       def self.encode(email)
-        key = (rand() * 256).to_i
-        hex = ('0' + key.to_s(16))[-2..-1]
-        email.split('').each do |char|
-          hex += ('0' + (char.ord ^ key).to_s(16))[-2..-1]
+        key = (rand * 256).to_i
+        hex = ("0" + key.to_s(16))[-2..]
+        email.chars.each do |char|
+          hex += ("0" + (char.ord ^ key).to_s(16))[-2..]
         end
         hex
       end
 
       def self.decode(encoded)
-        email, r, n = '', encoded[0..1].to_i(16), 2
+        email, r, n = "", encoded[0..1].to_i(16), 2
         loop do
           email += (encoded[n..(n + 1)].to_i(16) ^ r).chr
           n += 2
--- spec/cloudflare/email/protected_spec.rb
+++ spec/cloudflare/email/protected_spec.rb
@@ -1,20 +1,20 @@
 # frozen_string_literal: true
 
 RSpec.describe Cloudflare::Email::Protected do
-  describe '#encode' do
-    it 'should encode the specific email' do
-      hex1 = Cloudflare::Email::Protected.encode('username@domain.tld')
-      hex2 = Cloudflare::Email::Protected.encode('username@domain.tld')
+  describe "#encode" do
+    it "should encode the specific email" do
+      hex1 = Cloudflare::Email::Protected.encode("username@domain.tld")
+      hex2 = Cloudflare::Email::Protected.encode("username@domain.tld")
 
       expect(hex1).not_to eq(hex2)
     end
   end
 
-  describe '#decode' do
-    let(:hex1) { 'ff8a8c9a8d919e929abf9b90929e9691d18b939b' }
-    let(:hex2) { '285d5b4d5a4649454d684c4745494146065c444c' }
+  describe "#decode" do
+    let(:hex1) { "ff8a8c9a8d919e929abf9b90929e9691d18b939b" }
+    let(:hex2) { "285d5b4d5a4649454d684c4745494146065c444c" }
 
-    it 'should decode the specific email' do
+    it "should decode the specific email" do
       email1 = Cloudflare::Email::Protected.decode(hex1)
       email2 = Cloudflare::Email::Protected.decode(hex2)
 
